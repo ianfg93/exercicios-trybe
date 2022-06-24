29bae0a654c8a8b43f3f4db47b29b10fb0af8572
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-06-24 15:37:54 -0300

    exercicios de fixação

--- exerciciosObjeto.js
+++ exerciciosObjeto.js
@@ -13,5 +13,39 @@ for (let index = 0; index < player.bestInTheWorld.ano.length; index += 1) {
 }
 console.log("Nome: ", player["name"] + " ", player["lastName"]);
 console.log("Idade : ", player["age"]);
-console.log('A jogadora', player['name'] + ' ', 'foi eleita a melhor do mundo por', [anoMelhor] + ' ','vezes');
-console.log('A jogadora possui' , player.medals['golden'] + ' medalhas de ouro e ' + player.medals['silver'] + ' de prata');
+console.log(
+  "A jogadora",
+  player["name"] + " ",
+  "foi eleita a melhor do mundo por",
+  [anoMelhor] + " ",
+  "vezes"
+);
+console.log(
+  "A jogadora possui " + player.medals["golden"] + " medalhas de ouro e " + player.medals["silver"] + " de prata"
+);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+let names = {
+  person1: "João",
+  person2: "Maria",
+  person3: "Jorge",
+};
+
+for (let index in names) {
+  console.log("Olá ", names[index]);
+}
+
+let car = {
+  model: "A3 Sedan",
+  manufacturer: "Audi",
+  year: 2020,
+};
+for (let index in car) {
+  console.log(index + ' ' + car[index]);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  
